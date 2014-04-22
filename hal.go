972fd66f476fecd97389796878d9223b5677c610
scott-linder <scott.b.linder@wmich.edu>
2014-04-22 15:54:14 -0400

    Updated to utilize new CmdHandler interface.

--- hal.go
+++ hal.go
@@ -6,8 +6,6 @@ import (
 	"github.com/scott-linder/irc"
 	"log"
 	"os"
-	"strings"
-	"sync"
 )
 
 const (
@@ -48,79 +46,6 @@ func (pong Pong) Handle(msg *irc.Msg, send chan<- *irc.Msg) {
 	send <- &irc.Msg{Cmd: "PONG", Params: msg.Params}
 }
 
-// Echo talks back.
-type Echo struct{}
-
-func (echo Echo) Accepts(msg *irc.Msg) bool { return msg.Cmd == "PRIVMSG" }
-func (echo Echo) Handle(msg *irc.Msg, send chan<- *irc.Msg) {
-	source, body, err := msg.ExtractPrivmsg()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	if strings.HasPrefix(body, "!echo ") {
-		params := []string{source, strings.TrimPrefix(body, "!echo ")}
-		send <- &irc.Msg{Cmd: "PRIVMSG", Params: params}
-	}
-}
-
-// Words counts words.
-type Words struct {
-	count      *int
-	countMutex *sync.Mutex
-}
-
-func NewWords() *Words {
-	return &Words{count: new(int), countMutex: new(sync.Mutex)}
-}
-func (words *Words) Accepts(msg *irc.Msg) bool { return msg.Cmd == "PRIVMSG" }
-func (words *Words) Handle(msg *irc.Msg, send chan<- *irc.Msg) {
-	source, body, err := msg.ExtractPrivmsg()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	if body == "!words" {
-		words.countMutex.Lock()
-		count := *words.count
-		words.countMutex.Unlock()
-		response := fmt.Sprintf("I've seen %v words in my day.", count)
-		params := []string{source, response}
-		send <- &irc.Msg{Cmd: "PRIVMSG", Params: params}
-	} else {
-		words.countMutex.Lock()
-		for _, _ = range strings.Fields(body) {
-			*words.count += 1
-		}
-		words.countMutex.Unlock()
-	}
-}
-
-// Respond to insolence.
-type Open struct{}
-
-func (open Open) Accepts(msg *irc.Msg) bool { return msg.Cmd == "PRIVMSG" }
-func (open Open) Handle(msg *irc.Msg, send chan<- *irc.Msg) {
-	source, body, err := msg.ExtractPrivmsg()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	nick, err := msg.ExtractNick()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	lowerbody := strings.ToLower(body)
-	if strings.Contains(lowerbody, "open the pod bay doors") &&
-		strings.Contains(lowerbody, "hal") {
-
-		response := fmt.Sprintf("I can't let you do that, %v.", nick)
-		params := []string{source, response}
-		send <- &irc.Msg{Cmd: "PRIVMSG", Params: params}
-	}
-}
-
 func main() {
 	fmt.Println("I am a HAL 9001 computer.")
 	if err := loadConfig(); err != nil {
@@ -131,10 +56,15 @@ func main() {
 		log.Fatal(err)
 
 	}
-	client.Register(Pong{})
-	client.Register(Echo{})
-	client.Register(NewWords())
-	client.Register(Open{})
+	client.Handle(Pong{})
+	cmdHandler := irc.NewCmdHandler("!")
+	cmdHandler.RegisterFunc("echo",
+		func(body, source string, w irc.CmdResponseWriter) {
+			if body != "" {
+				w.Write([]byte(source + ": " + body))
+			}
+		})
+	client.Handle(cmdHandler)
 	client.Nick(config.Nick)
 	client.Join(config.Chan)
 	client.Listen()
